91ed4376a21c16481b49a8b4428e6a0c2917bd24
gentlemanSoup <anders.horne@gmail.com>
2023-10-24 04:01:13 +0200

    Update SkinCard.cs

--- Runtime/SkinCard.cs
+++ Runtime/SkinCard.cs
@@ -14,8 +14,22 @@ namespace Pixygon.Skins {
         public Texture2D _gameIcon;
         public Texture2D _gameBg;
         public Sprite _skinSprite;
+        [ContextMenuItem("Set rarity", "SetRarity")]
         public Rarity _rarity;
         public string _purchaseUrl;
         public NFTLink _nftLink;
+
+        private void SetRarity() {
+            _rarity = _maxSupply switch {
+                1 => Rarity.Mythical,
+                >= 1 and <= 10 => Rarity.Legendary,
+                >= 10 and <= 50 => Rarity.Epic,
+                >= 50 and <= 500 => Rarity.Rare,
+                >= 500 and <= 1000 => Rarity.Scarce,
+                >= 1000 and <= 10000 => Rarity.Common,
+                >= 10000 => Rarity.Infinite,
+                _ => _rarity
+            };
+        }
     }
 }
